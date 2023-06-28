15c78b142d6fb2e55e3f4a417f63ea034bda5d31
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-06-28 19:42:11 +0100

    developed logic to create user in signin widget

--- src/classes/SigninWidget.cpp
+++ src/classes/SigninWidget.cpp
@@ -1,4 +1,6 @@
 #include <QtWidgets/QMessageBox>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QUrlQuery>
 #include <QtDebug>
 #include "SigninWidget.h"
 #include "MainWindow.h"
@@ -31,7 +33,38 @@ void SigninWidget::CancelButtonClicked() {
 }
 
 void SigninWidget::SigninButtonClicked() {
-    QMessageBox msg;
-    msg.setText("Signin button");
-    msg.exec();
+    QLineEdit* usernameInput = this->ui.usernameInput;
+    QLineEdit* emailInput = this->ui.emailInput;
+    QLineEdit* passwordInput = this->ui.passwordInput;
+    QLineEdit* reinsertPasswordInput = this->ui.reinsertPasswordInput;
+
+    if ((*usernameInput).text().size() > 0 && (*emailInput).text().size() && (*passwordInput).text().size() > 0 && (*reinsertPasswordInput).text().size() > 0) {
+        if ((*passwordInput).text() == (*reinsertPasswordInput).text()) {
+            QNetworkAccessManager* manager = new QNetworkAccessManager(this);
+            QUrl url("https://food-organizer-backend.hopto.org/api/v1/users");
+            QNetworkRequest request(url);
+
+            request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+ 
+            QUrlQuery params;
+            params.addQueryItem("username", (*usernameInput).text());
+            params.addQueryItem("email", (*emailInput).text());
+            params.addQueryItem("password", (*passwordInput).text());
+            
+            manager->post(request, params.toString(QUrl::FullyEncoded).toUtf8());
+
+            QMessageBox msg;
+            msg.setText("User created successfully");
+            msg.exec();
+            CancelButtonClicked();
+        } else {
+            QMessageBox msg;
+            msg.setText("Passwords are not the same");
+            msg.exec();
+        };
+    } else {
+        QMessageBox msg;
+        msg.setText("Inputs are invalid");
+        msg.exec();
+    };
 }
