175a9464b6d44089545966fa8d191738dbe0826a
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-06-30 08:59:23 +0100

    developed basic get request in settings widget

--- src/classes/SettingsWidget.cpp
+++ src/classes/SettingsWidget.cpp
@@ -1,5 +1,10 @@
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QMessageBox>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QJsonObject>
+#include <QDebug>
+#include <fstream>
+#include <iostream>
 #include "SettingsWidget.h"
 
 SettingsWidget::SettingsWidget(QWidget* parent) : QWidget(parent) {
@@ -18,8 +23,15 @@ SettingsWidget::SettingsWidget(QWidget* parent) : QWidget(parent) {
     QNetworkAccessManager* manager = new QNetworkAccessManager(this);
     connect(manager, &QNetworkAccessManager::finished, this, &SettingsWidget::GetRequestFinished);
 
-    QUrl url("https://food-organizer-backend.hopto.org/api/v1/users/1");
+    std::ifstream authFile("build/auth.txt");
+    std::string token;
+    authFile >> token;
+    QString QToken = QString::fromStdString("Bearer " + token);
+
+    QUrl url("https://food-organizer-backend.hopto.org/api/v1/users/user");
     QNetworkRequest request(url);
+
+    request.setRawHeader(QByteArrayLiteral("Authorization"),QToken.toUtf8());
 
     manager->get(request);
 }
@@ -44,4 +56,16 @@ void SettingsWidget::DeleteButtonClicked() {
     QMessageBox msg;
     msg.setText("delete button");
     msg.exec();
+}
+
+void SettingsWidget::GetRequestFinished(QNetworkReply* reply) {
+    QByteArray responseData = reply->readAll();
+    QVariant responseStatus = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+
+    QJsonDocument json = QJsonDocument::fromJson(responseData);
+    QJsonObject replyObject = json.object();
+    reply->deleteLater();
+
+    qDebug() << responseStatus;
+    qDebug() << replyObject.value("email").toString();
 }
